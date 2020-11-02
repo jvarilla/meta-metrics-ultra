b722898e693b5cffbada4d52f158ce77761e5f2c
Shatilla Prayer <shatilla@Shatillas-iMac.local>
2020-11-02 01:35:24 -0800

    Added some test charts from reccharts

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
 import './App.css';
+import PieCharts from './components/PieCharts/PieCharts';
+import BarCharts from './components/BarCharts/BarCharts';
 
-function App() {
-  return (
-    <div className="App">
-      
+
+class App extends React.Component {
+  render(){
+    return (
+    <div class="container">
+     <div class="row">
+      <div class="col-md-6 col-sm-12">
+        <PieCharts/>
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <BarCharts/>
+      </div>
+     </div>
     </div>
   );
+  }
 }
 
 export default App;
